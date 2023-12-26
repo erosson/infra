ea8b8e0c6c737ef59bbf97d3e2dbaaa258282aa6
Evan Rosson <git@erosson.org>
2023-12-26 05:19:23 +0000

    better secrets management; add cloudflare tf backend

--- tf/main.tf
+++ tf/main.tf
@@ -9,6 +9,25 @@ terraform {
       version = "4.20.0"
     }
   }
+
+  // Not actually amazon S3. cloudflare R2 - aws-S3-compatible.
+  // different auth token than the rest of cloudflare: https://dash.cloudflare.com/fed1bc67ffb2f62c6deaa5c7f8f9eb93/r2/api-tokens
+  // directions here: https://github.com/hashicorp/terraform/issues/33847#issuecomment-1854605813
+  backend "s3" {
+    // bucket content: https://dash.cloudflare.com/fed1bc67ffb2f62c6deaa5c7f8f9eb93/r2/default/buckets/erosson-infra
+    bucket = "erosson-infra"
+    key    = "erosson/infra/terraform.tfstate"
+    endpoints = { s3 = "https://fed1bc67ffb2f62c6deaa5c7f8f9eb93.r2.cloudflarestorage.com" }
+    region = "us-east-1"
+
+    # access_key = var.CLOUDFLARE_R2_BACKEND_ACCESS_KEY
+    # secret_key = var.CLOUDFLARE_R2_BACKEND_SECRET_KEY
+    skip_credentials_validation = true
+    skip_region_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum = true
+  }
 }
 
 provider "github" {}
