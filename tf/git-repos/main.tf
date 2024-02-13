45b25adbfdf7d2258af3f119f6d31fe854819a0b
Evan Rosson <git@erosson.org>
2024-02-13 09:23:20 +0000

    docs and wolcendb-repo

--- tf/git-repos/main.tf
+++ tf/git-repos/main.tf
@@ -1,10 +1,10 @@
 # https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository
-# $ terraform import github_repository.terraform terraform
+# $ ./tofu import github_repository.terraform terraform
 
 resource "github_repository" "test-tf_erosson_org" {
   # https://github.com/erosson/test-tf.erosson.org
   name = "test-tf.erosson.org"
-  homepage_url = "test-tf.erosson.org"
+  homepage_url = "https://test-tf.erosson.org"
   license_template = "GPL-3.0"
   lifecycle {
     # It's actually safe to recreate this specific repo, as it has no commit history other than what terraform does to it.
@@ -15,7 +15,16 @@ resource "github_repository" "test-tf_erosson_org" {
 resource "github_repository" "math2_swarmsim_com" {
   # https://github.com/erosson/math2.swarmsim.com
   name = "math2.swarmsim.com"
-  homepage_url = "math2.swarmsim.com"
+  homepage_url = "https://math2.swarmsim.com"
+  license_template = "GPL-3.0"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+resource "github_repository" "wolcendb" {
+  # https://github.com/erosson/wolcendb
+  name = "wolcendb"
+  homepage_url = "https://wolcendb.erosson.org"
   license_template = "GPL-3.0"
   lifecycle {
     prevent_destroy = true
--- tf/test-tf.erosson.org/README.md
+++ tf/test-tf.erosson.org/README.md
@@ -1,3 +1,7 @@
+https://test-tf.erosson.org
+
+https://github.com/erosson/test-tf.erosson.org
+
 A simple static site created entirely by Terraform.
 
 Safe to destroy/recreate.
