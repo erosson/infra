d33b0fbc3fe338cdb951237843fd1f3590a4a7c4
Evan Rosson <git@erosson.org>
2024-02-13 08:20:48 +0000

    pre-push hook

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eux
 
+cp -f git-hooks/* .git/hooks/
+
 wget -O /tmp/dnscontrol.deb https://github.com/StackExchange/dnscontrol/releases/download/v4.7.3/dnscontrol-4.7.3.amd64.deb
 sudo dpkg -i /tmp/dnscontrol.deb
 rm -f /tmp/dnscontrol.deb
