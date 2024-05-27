da6c66247a53a3f62833c12216445827300d150d
Evan Rosson <git@erosson.org>
2024-05-27 21:11:14 +0000

    more redirect testing

--- tests/redirect.test.ts
+++ tests/redirect.test.ts
@@ -6,24 +6,33 @@ interface RedirectData {
     status?: number
 }
 const redirects: readonly RedirectData[] = [
-
     ...selfRedirect('erosson.org'),
     ...selfRedirect404('erosson.org'),
     ...domainRedirect('erosson.org', 'erosson.com'),
     ...domainRedirect404('erosson.org', 'erosson.com'),
-    // ...domainRedirect('https://www.erosson.org/', 'evanrosson.com'),
-    // ...domainRedirect('https://www.erosson.org/', 'evanrosson.org'),
-    // ...domainRedirect('https://www.erosson.org/', 'erosson.us'),
+    ...domainRedirect('erosson.org', 'evanrosson.com'),
+    ...domainRedirect404('erosson.org', 'evanrosson.com'),
+    ...domainRedirect('erosson.org', 'evanrosson.org'),
+    ...domainRedirect404('erosson.org', 'evanrosson.org'),
+    ...domainRedirect('erosson.org', 'erosson.us'),
+    ...domainRedirect404('erosson.org', 'erosson.us'),
 
     ...selfRedirect('swarmsim.com'),
-    // ...domainRedirect('swarmsim.com', 'swarmsimulator.com'),
+    // ...selfRedirect404('swarmsim.com'),
+    ...domainRedirect('swarmsim.com', 'swarmsimulator.com'),
+    // ...domainRedirect404('swarmsim.com', 'swarmsimulator.com'),
 
-    // ...domainRedirect('warswarms.com', 'war-swarms.com'),
+    ...domainRedirect('warswarms.com', 'war-swarms.com'),
+    ...domainRedirect404('warswarms.com', 'war-swarms.com'),
     ...domainRedirect('warswarms.com', 'warswarm.com'),
+    ...domainRedirect404('warswarms.com', 'warswarm.com'),
 
     ...selfRedirect('zealgame.com'),
 ]
 
+/**
+ * Verify "http://example.com", "http://www.example.com", and "https://example.com" all redirect to "https://www.example.com"
+ */
 function selfRedirect(domain: string): readonly RedirectData[] {
     const to = `https://www.${domain}/`
     return [
@@ -46,9 +55,9 @@ function domainRedirect(toDomain: string, from: string): readonly RedirectData[]
     const to = `https://www.${toDomain}/`
     return [
         { to, from: `http://${from}/` },
-        // { to, from: `http://www.${from}/` },
+        { to, from: `http://www.${from}/` },
         { to, from: `https://${from}/` },
-        // { to, from: `https://www.${from}/` },
+        { to, from: `https://www.${from}/` },
     ]
 }
 function domainRedirect404(toDomain: string, from: string): readonly RedirectData[] {
@@ -56,9 +65,9 @@ function domainRedirect404(toDomain: string, from: string): readonly RedirectDat
     const to404 = `${to.replace(/\/$/, '')}/404`
     return [
         { to: to404, status: 404, from: `http://${from}/404` },
-        // { to: to404, status: 404, from: `http://www.${from}/404` },
+        { to: to404, status: 404, from: `http://www.${from}/404` },
         { to: to404, status: 404, from: `https://${from}/404` },
-        // { to: to404, status: 404, from: `https://www.${from}/404` },
+        { to: to404, status: 404, from: `https://www.${from}/404` },
     ]
 }
 
